57a72b025219624134c337f1626d65852ffa7a9c
did9984 <113024115+did9984@users.noreply.github.com>
2023-01-09 15:03:51 +0900

    import

---
+++ chap02/src/com/yedam/variable/Import.java
@@ -0,0 +1,25 @@
+package com.yedam.variable;
+
+import java.util.Scanner;
+
+public class Import {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in); // system.in 은데이터 입력	
+		System.out.println("Write your name");
+		String name = input.nextLine();	
+		System.out.println("How old are you?");
+		int number = input.nextInt();// javascript value 같은 의미 값을 불러옴
+		input.nextLine();
+		System.out.println("What is your favorite food?");
+		String food = input.nextLine();
+		
+		System.out.println("hello" + name);
+		System.out.println("your age is:" + number);
+		System.out.println("You like "+food);
+
+	}
+
+}
