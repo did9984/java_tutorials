2a42ee38c1acb7ae77d3038144625f528e9472d9
did9984 <113024115+did9984@users.noreply.github.com>
2023-01-09 16:18:28 +0900

    thrid

---
+++ com.yedam.print/src/com/yedam/print/printExample.java
@@ -0,0 +1,15 @@
+package com.yedam.print;
+
+public class printExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      // println
+		System.out.println("출력과 동시에 행을 변경(Enter)");
+		//pritn
+		System.out.print("출력만 함. \n");
+		//printf
+		System.out.printf("이름:%s,나이%d,온도:%f","park",28,9.5);
+	}
+
+}
