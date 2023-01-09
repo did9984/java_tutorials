8de190e3563bf4f58901e15620b6313b4f5affcb
did9984 <113024115+did9984@users.noreply.github.com>
2023-01-09 15:05:14 +0900

    variable

---
+++ chap02/src/com/yedam/variable/Varibable2.java
@@ -0,0 +1,18 @@
+package com.yedam.variable;
+
+public class Varibable2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int v1 = 15; // 중괄호 안에서 선언된 변수는 중괄호 안에서만 가능하다 
+		
+		if (v1 > 10) {
+			int v2;
+			v2 = v1 - 10;
+	
+		}
+		
+		int v3 = v1 + + 5;
+	}
+
+}
