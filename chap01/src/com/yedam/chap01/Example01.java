ff515dfc3bd1ddad25ddb343513e9b6c9da82c3e
did9984 <113024115+did9984@users.noreply.github.com>
2023-01-09 15:53:36 +0900

    test commit

---
+++ chap01/src/com/yedam/chap01/Example01.java
@@ -0,0 +1,11 @@
+package com.yedam.chap01;
+
+public class Example01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("한글");
+  
+	}
+
+}
---
+++ chap01/src/com/yedam/chap01/OparatorCastion.java
@@ -0,0 +1,55 @@
+package com.yedam.chap01;
+
+public class OparatorCastion {
+  public static void main(String[] args) {
+	byte value1= 10; 
+	int value2 = 100;
+	long value3 = 1000l;
+	
+	long result = value1+value2+value3;
+	System.out.println(result);
+	
+	int intValue1 = 10;
+	int intValue4 = intValue1/4;
+	System.out.println(intValue4);
+	
+	int intValue2 = 10;
+	double intValue3 = intValue2 /4.0;
+  System.out.println(intValue3);
+  
+  int x = 1; 
+  int y = 2; 
+  double result2 = x/(double)y;
+  System.out.println(result2);
+  
+  //+ 연산 
+  //숫자 연산 
+  int value = 10+2+8;
+  System.out.println(value);
+  
+  //문자열 결합 연산 
+  String str1 = 10+2+"8";
+  System.out.println("str1"+str1);
+  
+  String str2 = 10+"2"+8;
+  System.out.println("str2:" +str2);
+  
+  
+  String str3 = "10"+2+8;
+  System.out.println("str3:" +str3);
+  
+  String str4 = "10"+(2+8);
+  System.out.println("str4:" +str4);
+  
+  //String <->정수,실수 타입변환 
+  int valueStr = Integer.parseInt(str4);
+  System.out.println(valueStr);
+//  byte byteValue = Byte.parseByte(str4);
+  short shortValue = Short.parseShort(str4);
+  long longValue = Long.parseLong(str4);
+  double dValue = Double.parseDouble(str4);
+  System.out.println(dValue);
+//  boolean bValue = Boolean.parseBoolean(str4);
+
+}
+}
